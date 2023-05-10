0694ac8acd72a02778b7548f6e1dd1caf769dbdb
Lhays Izidoro <izidorolhays@gmail.com>
2023-05-09 21:34:02 -0300

    merge

--- src/App.js
+++ src/App.js
@@ -8,22 +8,6 @@ import Contato from './pages/contato/Contato';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Oi meninas :)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
       <Router>
         <Header />
 
@@ -33,7 +17,6 @@ function App() {
           <Route path='/contato' element={<Contato />} />
         </Routes>
       </Router>
->>>>>>> b503cfb9d63f9fadf12ab2a57f9a18043c105bca
     </div>
   );
 }
